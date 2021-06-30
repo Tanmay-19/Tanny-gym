ee458e2cdf45272f86bb0a091379a44b58e5424d
Tanmay Chauhan <65083025+Tanmay-19@users.noreply.github.com>
2021-06-30 21:48:08 +0530

    Add files via upload

---
+++ tanny-gym-master/script.js
@@ -0,0 +1,6 @@
+const toggle = document.querySelector('.toggle');
+        const main = document.querySelector('.main');
+        toggle.addEventListener('click', () => {
+            toggle.classList.toggle('active');
+            main.classList.toggle('active');
+        })
